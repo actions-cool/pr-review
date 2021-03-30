4cf5f77e28f519fbd49d4d538899685e29522a6e
ajuner <106791576@qq.com>
2021-03-30 16:10:55 +0800

    update

--- src/main.js
+++ src/main.js
@@ -7,8 +7,7 @@ async function run() {
     const token = core.getInput('token');
     const octokit = new Octokit({ auth: `token ${token}` });
     const context = github.context;
-    console.log('token', token);
-    console.log('eventName', context.eventName);
+    core.info(`token: ${token},eventName: ${context.eventName}`);
     const { owner, repo } = context.repo;
     const number = context.payload.pull_request.number;
 
