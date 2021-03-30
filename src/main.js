4fa89d044c464a49795f06c6093c3e29c614808f
ajuner <106791576@qq.com>
2021-03-30 15:56:51 +0800

    chore: use pr-approve

--- src/main.js
+++ src/main.js
@@ -2,8 +2,6 @@ const core = require('@actions/core');
 const { Octokit } = require('@octokit/rest');
 const github = require('@actions/github');
 
-const events = ['APPROVE', 'REQUEST_CHANGES', 'COMMENT'];
-
 async function run() {
   try {
     const token = core.getInput('token');
@@ -12,16 +10,14 @@ async function run() {
 
     const { owner, repo } = context.repo;
     const number = context.payload.pull_request.number;
-    console.log(context.eventName)
-    const approve_body = core.getInput('approve_comment');
 
     await octokit.pulls.createReview({
       owner,
       repo,
       pull_number: number,
-      event: events[0],
-      body: approve_body,
-    });
+      event: 'APPROVE',
+      body: '1234',
+    })
   } catch (error) {
     core.setFailed(error.message);
   }
